179d8afc3f9adf619731a2dd0187ef29cc0d67ec
naraemirae9 <naraemirae9@naver.com>
2020-07-24 10:25:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/instagram_future/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.instagram_future;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
